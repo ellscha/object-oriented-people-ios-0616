a739c011e547e209199575113eaf9be9c6d78ae8
ellscha <escharlin@wesleyan.edu>
2016-06-13 11:53:53 -0400

    working on finishing methods, but all have been written to the basics. Finished Celebrate Birthday. (might go in later to create a method to decide whether it should be 'th' or 'st' or 'nd' after the number, but not yet.

---
+++ ObjectOrientedPeople/FISPerson.m
@@ -0,0 +1,89 @@
+//
+//  FISPerson.m
+//  ObjectOrientedPeople
+//
+//  Created by Elli Scharlin on 6/13/16.
+//  Copyright © 2016 al-tyus.com. All rights reserved.
+//
+
+#import "FISPerson.h"
+@interface FISPerson ()
+@property (strong, nonatomic, readwrite) NSString *name;
+@property (nonatomic, readwrite) NSUInteger ageInYears;
+@property (nonatomic, readwrite) NSUInteger heightInInches;
+@property (strong, nonatomic, readwrite) NSMutableArray *skills;
+@end
+
+@implementation FISPerson
+//default
+-(instancetype) init {
+    self = [self initWithName:@"Elli" ageInYears:20 heightInInches:64];
+    return self;
+}
+//convenience
+-(instancetype)initWithName:(NSString *)name ageInYears:(NSUInteger)ageInYears{
+    self = [self initWithName:name ageInYears:ageInYears heightInInches:67];
+    return self;
+}
+//designated
+-(instancetype)initWithName:(NSString *)name ageInYears:(NSUInteger)ageInYears heightInInches:(NSUInteger)heightInInches{
+    self = [super init];
+    if (self){
+        _name = name;
+        _ageInYears = ageInYears;
+        _heightInInches = heightInInches;
+        _skills = [[NSMutableArray alloc]init];
+    }
+    return self;
+}
+
+
+-(NSString *)celebrateBirthday{
+    self.ageInYears += 1;
+    [self ordinalForInteger:self.ageInYears];
+    
+    //Make a method for TH
+    NSString *birthdayMessage = [NSString stringWithFormat:@"HAPPY %luTH BIRTHDAY, %@!!!", self.ageInYears, [self.name capitalizedString]];
+    NSLog(@"%@", birthdayMessage);
+    return birthdayMessage;
+    
+    
+    return @"";
+}
+- (NSString *)ordinalForInteger:(NSUInteger)integer {
+    NSString *ordinal = @"th";
+    if (integer % 10 == 1 && integer % 100 != 11) {
+        ordinal = @"st";
+    } else if (integer % 10 == 2 && integer % 100 != 12) {
+        ordinal = @"nd";
+    } else if (integer % 10 == 3 && integer % 100 != 13) {
+        ordinal = @"rd";
+    }
+    return ordinal;
+}
+
+-(void)learnSkillBash{
+    
+}
+
+-(void)learnSkillInterfaceBuilder{
+    
+}
+
+-(void)learnSkillObjectiveC{
+    
+}
+
+-(void)learnSkillObjectOrientedProgramming{
+    
+}
+
+-(void)learnSkillXcode{
+    
+}
+
+-(BOOL)isQualifiedTutor{
+    return NO;
+}
+
+@end
